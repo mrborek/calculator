300a2e67021343d1d8989f4d4341e30569da09a9
ab <ab@101lab.pl>
2025-08-04 10:27:42 +0200

    Refactor `Calculator` class to improve code readability and reuse with `formatResult` helper function.

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,36 @@
-interface CalculatorOptions{
-    precision?: number
+interface CalculatorOptions {
+  precision?: number;
 }
 
-export default class Calculator{
-    precision:number|undefined = undefined
+export default class Calculator {
+  precision: number | undefined = undefined;
 
-    constructor(options? :CalculatorOptions){
-        if(options?.precision)
-            this.precision = options?.precision
+  constructor(options?: CalculatorOptions) {
+    if (options?.precision) this.precision = options?.precision;
+  }
+
+  private formatResult(value: number): number | string {
+    return this.precision !== undefined
+      ? parseFloat(value.toFixed(this.precision))
+      : value;
+  }
+
+  add(a: number, b: number) {
+    return this.formatResult(a + b);
+  }
+
+  subtract(a: number, b: number) {
+    return this.formatResult(a - b);
+  }
+
+  multiply(a: number, b: number) {
+    return this.formatResult(a * b);
+  }
+
+  divide(a: number, b: number) {
+    if (b === 0) {
+      throw new Error("Error: division by zero is not allowed");
     }
-
-
-    add(a: number, b: number){
-        return this.precision ? (a + b).toFixed(this.precision): a + b
-    }
-
-    subtract(a: number, b: number){
-        return this.precision ? (a - b).toFixed(this.precision): a - b
-    }
-
-    multiply(a: number, b: number){
-        return this.precision ? (a * b).toFixed(this.precision): a * b
-    }
-
-    divide(a: number, b: number){
-        if(b === 0){
-            throw new Error('Error: division by zero is not allowed')
-        }
-        return this.precision ? (a / b).toFixed(this.precision): a / b
-    }
+    return this.formatResult(a / b);
+  }
 }
