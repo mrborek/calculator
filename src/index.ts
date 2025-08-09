5ae0345ba867a45cabd07bcf224f3ddbe7373a6a
mrborek <artur.a.borek@gmail.com>
2025-08-09 19:32:05 +0200

    extend calculations

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,11 @@ interface CalculatorOptions {
 export default class Calculator {
   precision: number | undefined = undefined;
 
+  total: number = 0;
+
   constructor(options?: CalculatorOptions) {
     if (options?.precision) this.precision = options?.precision;
+
   }
 
   private formatResult(value: number): number | string {
@@ -15,22 +18,70 @@ export default class Calculator {
       : value;
   }
 
-  add(a: number, b: number) {
-    return this.formatResult(a + b);
-  }
-
-  subtract(a: number, b: number) {
-    return this.formatResult(a - b);
-  }
-
-  multiply(a: number, b: number) {
-    return this.formatResult(a * b);
-  }
-
-  divide(a: number, b: number) {
-    if (b === 0) {
-      throw new Error("Error: division by zero is not allowed");
+  private validateInput(a: number, b?: number){
+    if(a === undefined && b){
+      throw new Error("missing first number");
     }
-    return this.formatResult(a / b);
+  }
+
+  add(a: number, b?: number) {
+    this.validateInput(a, b)
+
+    if (a && b)
+      this.total = a + b
+
+    if (a && !b)
+      this.total = this.total + a
+    
+    return this
+  }
+
+  subtract(a: number, b?: number) {
+    this.validateInput(a, b)
+
+    if (a && b)
+      this.total = a - b
+
+    if (a && !b)
+      this.total = this.total - a
+  
+    return this
+  }
+
+  multiply(a: number, b?: number) {
+    this.validateInput(a, b)
+
+    if (a && b)
+      this.total = a * b
+
+    if (a && !b)
+      this.total = this.total * a
+
+    return this
+  }
+
+  divide(a: number, b?: number) {
+    this.validateInput(a, b)
+
+    if (a === 0 || b === 0) {
+      throw new Error("division by zero is not allowed");
+    }
+    if (a && b)
+      this.total = a / b
+
+    if (a && !b)
+      this.total = this.total / a
+
+    return this
+  }
+
+  clear(){
+    this.total = 0
+
+    return this
+  }
+
+  getTotal(){
+    return this.formatResult(this.total)
   }
 }
--- src/playground.ts
+++ src/playground.ts
@@ -7,10 +7,16 @@ const num1 = 5;
 const num2 = 3;
 
 const results = {
-    add: calculator.add(num1,num2),
-    subtract: calculator.subtract(num1,num2),
-    multiply: calculator.multiply(num1,num2),
-    divide: calculator.divide(num1,num2),
+    add: calculator.add(num1,num2).getTotal(),
+    subtract: calculator.subtract(num1,num2).getTotal(),
+    multiply: calculator.multiply(num1,num2).getTotal(),
+    divide: calculator.divide(num1,num2).getTotal(),
 }
 
 console.log(num1, num2, results)
+
+const calc2 = new Calculator({precision: 2})
+
+calc2.add(3,3).add(4, 6).subtract(5).multiply(5).divide(2)
+
+console.log('calc2.total', calc2.total)
